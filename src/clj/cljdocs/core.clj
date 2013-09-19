f2daf6df4ad03a893949f57c15f5b057fd17ccbc
Marek Kubica <marek@xivilization.net>
2013-09-19 23:34:45 +0200

    Serving static pages on root URL

--- src/clj/cljdocs/core.clj
+++ src/clj/cljdocs/core.clj
@@ -1,12 +1,20 @@
 (ns cljdocs.core
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :as response]))
 
 (defroutes app-routes
-  (GET "/" [] "<p>Hello from compojure</p>")
+  ;(GET "/" [] "<p>Hello from compojure</p>")
   (route/resources "/")
   (route/not-found "Page not found"))
 
+(defn wrap-dir-index [handler]
+  (fn [req]
+    (handler
+     (update-in req [:uri]
+                #(if (= "/" %) "/index.html" %)))))
+
 (def handler
-  (handler/site app-routes))
+  (-> (handler/site app-routes)
+      (wrap-dir-index)))
