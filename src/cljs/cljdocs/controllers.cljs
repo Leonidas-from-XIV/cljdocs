cc4467df7c7de698dd8366cbab6cc35a22e7996c
Marek Kubica <marek@xivilization.net>
2013-09-20 19:51:51 +0200

    Next part of Angular tutorial

--- src/cljs/cljdocs/controllers.cljs
+++ src/cljs/cljdocs/controllers.cljs
@@ -2,19 +2,11 @@
 ;(def phonemvc
 ;  (.module js/angular "phonemvc" (array)))
 
-(defn ^:export phone-list-ctrl [$scope]
-  (set! (.-phones $scope) (clj->js
-        [
-         {:name "Nexus S"
-          :snippet "Fast just got faster with Nexus S."
-          :age 0}
-         {:name "Motorola XOOM with Wi-Fi"
-          :snippet "The Next, Next Generation tablet."
-          :age 1}
-         {:name "Motorola XOOM"
-          :snippet "The Next, Next Generation tablet."
-          :age 2}])))
-
+(defn ^:export phone-list-ctrl [$scope $http]
+  (.success (.get $http "phones/phones.json")
+            (fn [data]
+              (set! (.-phones $scope) data)))
+  (set! (.-orderProp $scope) "age"))
 
 ;(.controller phonemvc "PhoneListCtrl" phone-list-ctrl)
 
